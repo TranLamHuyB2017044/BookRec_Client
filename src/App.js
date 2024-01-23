77c38dd827abe8302833acaf4b484b1823386105
HuyLam <tranlamhuy5tn@gmail.com>
2024-01-23 21:57:50 +0700

    fix quote component

--- src/App.js
+++ src/App.js
@@ -17,35 +17,42 @@ import YourOrder from "./pages/ManagerAccountPage/YourOrder";
 import ChangePassword from "./pages/ManagerAccountPage/ChangePassword";
 
 function App() {
-  const user = true
-  const ProtectedRoute = () => {
-    if (user) {
-      return <Navigate to='/' replace />;
-    }
-  
-    return <Outlet />;
-  };
-  const ProtectUser = () => {
-    if(!user){
-      return <Navigate to='/login' replace />;
-    }
-    return <Outlet />;
-  }
+  // const user = true
+  // const ProtectedRoute = () => {
+  //   if (user) {
+  //     return <Navigate to='/' replace />;
+  //   }
+
+  //   return <Outlet />;
+  // };
+  // const ProtectUser = () => {
+  //   if(!user){
+  //     return <Navigate to='/login' replace />;
+  //   }
+  //   return <Outlet />;
+  // }
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route element={<ProtectedRoute/>}>
+        {/* <Route element={<ProtectedRoute/>}>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-        </Route>
-        <Route element={<ProtectUser/>}>
+        </Route> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* <Route element={<ProtectUser/>}>
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/account" element={<Account />} />
           <Route path="/yourOrders" element={<YourOrder />} />
           <Route path="/changepassword" element={<ChangePassword />} />
-        </Route>
+        </Route> */}
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/yourOrders" element={<YourOrder />} />
+        <Route path="/changepassword" element={<ChangePassword />} />
         <Route path="/collections" element={<BooksList />} />
         <Route path="/collections/:id" element={<BookDetail />} />
         <Route path="*" element={<NotFound />} />
--- src/Components/QuoteComponent/Quote.jsx
+++ src/Components/QuoteComponent/Quote.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
 import {Fade} from 'react-slideshow-image'
 export default function Quote() {
   return (
-    <div className=' h-[350px]  mt-5 bg-[#18335e] text-white'>
+    <div className=' h-[350px]   mt-5 bg-[#18335e] text-white'>
       <Fade arrows={false} duration={5000}>
-        <div className='each-slide max-w-[1200px] mx-auto mt-60 flex flex-col gap-16'>
+        <div className='each-slide  mx-auto mt-60 flex flex-col gap-16 h-[350px]'>
           <p className='text-3xl text-center italic leading-relaxed'>“Chính từ sách mà những người khôn ngoan tìm được sự an ủi khỏi những rắc rối của cuộc đời.”</p>
           <p className='font-bold text-center text-3xl'>Victor Hugo</p>
         </div>
-        <div className='each-slide max-w-[1200px] mx-auto mt-60 flex flex-col gap-16'>
+        <div className='each-slide  mx-auto mt-60 flex flex-col gap-16 h-[350px]'>
           <p className='text-3xl text-center italic leading-relaxed'>“Tôi tin rằng, nếu có một thứ mà càng mua nhiều lại càng giàu, thì đó là sách. Đọc xong một cuốn sách, nếu nó không giúp bạn giàu lên về mặt tài chính, thì cũng là về mặt trí tuệ. Mà chẳng phải trí tuệ vốn là nguồn gốc của mọi tài sản quý giá nhất mà bạn có thể thấy trên đời hay sao?”</p>
           <p className='font-bold text-center text-3xl'>Khiết Danh</p>
         </div>
-        <div className='each-slide max-w-[1200px] mx-auto mt-60 flex flex-col gap-16'>
+        <div className='each-slide  mx-auto mt-60 flex flex-col gap-16 h-[350px]'>
           <p className='text-3xl text-center italic leading-relaxed'>“Người yêu có thể bỏ ta đi, nhưng sách thì ở lại mãi mãi.”</p>
           <p className='font-bold text-center text-3xl'>Khiết Danh</p>
         </div>
